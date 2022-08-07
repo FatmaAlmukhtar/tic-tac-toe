39da392245e1bf8fef98b5a794e9b288b2f78ea8
Fatma Almukhtar <fatma.almukhtar18@gmail.com>
2022-08-06 19:30:42 -0700

    complete game functionality

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ const player = (playerName, sign) => {
 
 // module
 const gameBoard = (() => {
-    const board = new Array(9);
+    let board = ['','','','','','','','',''];
 
     const markSquare = (index, marker) => {
         board[index] =  marker;
     }
-    
+    const resetBoard = () => {
+        for(let i=0; i<board.length; i++) {
+            board[i] = '';
+        }
+        /*
+        board.map((element,index) => {
+            board[index] = '';
+        });*/
+    }
     const winCombination = [
         [0,1,2],
         [3,4,5],
@@ -21,7 +29,7 @@ const gameBoard = (() => {
         [0,4,8],
         [2,4,6]
     ]
-    return {markSquare, board, winCombination};
+    return {markSquare, board, winCombination, resetBoard};
 })();
 
 const gameController = (() => {
@@ -31,10 +39,21 @@ const gameController = (() => {
     let currentPlayer;
     let counter = 0
 
+    const resultDiv = document.querySelector('.result');
+    const resultMessage = document.querySelector('.message');
+    const restartButton = document.querySelector('.restartButton');
+
     const setCurrentPlayer = (counter) => {
         return counter % 2 === 0 ? currentPlayer = player_1 : currentPlayer = player_2;
     }
+    const checkGameEnd = () => {
+        return gameBoard.board.every(element => {
+            return element === 'x' || element === 'o';
+        })
+        
+    }
     const checkWin = () => {
+        
         return gameBoard.winCombination.some(combination => {
             return combination.every(index => {
                 return (gameBoard.board[index] === player_1.sign);
@@ -43,23 +62,42 @@ const gameController = (() => {
             })  
         })
     }
-    const checkDraw = () => {
-
-    } 
-    const play= () => {
-
+    const resetGame = () => {
+        resultDiv.classList.remove('display');
+        gameBoard.resetBoard();
+        counter = 0;
+        squares.forEach(square => {
+                square.innerHTML = '';
+                square.removeEventListener('click', play);
+                square.addEventListener('click', play, {once: true});
+        });
+    }
+    const checkStatus = () => {
+        
+        if(checkWin()) {
+            resultDiv.classList.add('display');
+            resultMessage.innerText = `${currentPlayer.sign}'s Wins!`;
+            restartButton.addEventListener('click', resetGame);
+        }
+        else if (checkGameEnd()) {
+            if(checkWin()) resultMessage.innerText = `${currentPlayer.sign}'s Wins!`;
+                
+            else resultMessage.innerText = 'Draw!';
+       
+            resultDiv.classList.add('display');
+            restartButton.addEventListener('click', resetGame);
+        }
+        else setCurrentPlayer(counter);
+    }
+    const play = (e) => {
+        gameBoard.markSquare(e.target.dataset.index, setCurrentPlayer(counter).sign);
+        counter++;
+        e.target.innerHTML = currentPlayer.sign;
+        checkStatus();
     }
     squares.forEach(square => {
-        square.addEventListener('click', () => {
-            gameBoard.markSquare(square.dataset.index, setCurrentPlayer(counter).sign);
-            counter++;
-            console.log(gameBoard.board);
-            square.innerHTML = currentPlayer.sign;
-            console.log(checkWin());
-        }, {once: true});        
+        square.addEventListener('click', play, {once: true});        
     });
     
-    
-    
-    return {setCurrentPlayer, checkWin}
+    return {play}
 })();
